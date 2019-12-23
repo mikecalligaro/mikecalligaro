90d1f80d467c8f52744ccc3be95e608421c1ebad
mikecalligaro <40806595+mikecalligaro@users.noreply.github.com>
2019-12-23 10:24:26 -0800

    Functions part 1

--- rust/functions/src/main.rs
+++ rust/functions/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    another_function(3, 4);
+
+    let z = plus_one(11);
+
+    if z % 4 == 0 {
+        println!("z is divisible by 4")
+    } else if z % 3 == 0 {
+        println!("z is divisible by 3")
+    } else {
+        println!("The value of z, {}, isn't divisible by either 4 or 3.", z);
+    }
+    
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("The values of x and y are: {} and {}", x, y);
+}
+
+// fn that returns an i32
+fn plus_one(x: i32) -> i32 {
+    x + 1  //Note no semicolon, so this is an expression.  Last expression is returned.
 }
