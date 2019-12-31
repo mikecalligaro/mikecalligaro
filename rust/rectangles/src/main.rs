79330eebc695bd78b92001ae581a4aa10fa05796
mikecalligaro <40806595+mikecalligaro@users.noreply.github.com>
2019-12-31 15:09:54 -0800

    rect update

--- rust/rectangles/src/main.rs
+++ rust/rectangles/src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     variables();
     tuples();
+    structs();
+
+    methods();
 }
 
 fn variables() {
@@ -28,4 +31,38 @@ fn tuples() {
 
 fn area_tup(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }   
+    
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn structs() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!(
+        "Structs: The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+}
+
+
+fn methods() {
+    let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
