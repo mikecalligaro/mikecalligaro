ee126b7cb898c54f517ea36525d810396d6949e4
mikecalligaro <40806595+mikecalligaro@users.noreply.github.com>
2020-08-29 10:56:44 -0700

    scaffolding for JS

--- interviews/tictactoe/js/ttt/ttt.js
+++ interviews/tictactoe/js/ttt/ttt.js
@@ -1,1 +1,9 @@
- 
+function RunCode() {
+    Output("Hello World ");
+    Output("Second Line");
+} 
+
+function Output(out) {
+    var element = document.getElementById("output");
+    element.innerHTML += out;
+}
